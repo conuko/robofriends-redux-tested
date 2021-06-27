cf8b77570e4b95921524081316ecca0b44f249d3
Constantin Unterkofler <constantinu93@gmail.com>
2021-06-27 15:06:24 +0200

    fix: change action.test.js

--- src/action.test.js
+++ src/action.test.js
@@ -6,21 +6,25 @@ import thunkMiddleware from 'redux-thunk';
 
 const mockStore = configureMockStore([thunkMiddleware]);
 
-it('should create an action to search Robots', () => {
-    const text = 'woooo';
-    const expectedAction = {
-        type: CHANGE_SEARCHFIELD,
-        payload: text
-    }
-    expect(actions.setSearchField(text)).toEqual(expectedAction);
+describe('actions', () => {
+    it('should create an action to search Robots', () => {
+        const text = 'woooo';
+        const expectedAction = {
+            type: CHANGE_SEARCHFIELD,
+            payload: text
+        }
+        expect(actions.setSearchField(text)).toEqual(expectedAction);
+    });
 });
 
-it('handles requesting robots API', () => {
-    const store = mockStore();
-    store.dispatch(actions.requestRobots());
-    const action = store.getActions();
-    const expectedAction = {
-        type: REQUEST_ROBOTS_PENDING,
-    };
-    expect(action[0]).toEqual(expectedAction);
+describe('Fetch robots action PENDING', () => {
+    it('should create a PENDING action on request Robots', () => {
+        const store = mockStore();
+        store.dispatch(actions.requestRobots());
+        const action = store.getActions();
+        const expectedAction = {
+            type: REQUEST_ROBOTS_PENDING,
+        };
+        expect(action[0]).toEqual(expectedAction);
+    });
 });
